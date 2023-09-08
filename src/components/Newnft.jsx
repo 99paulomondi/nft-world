f6d1ba76a71693d00a067fe4d8826257a3a8c705
paul omondi <99paulomondi@gmail.com>
2023-09-08 08:07:38 -0700

    working on the final touches of the home pahge

---
+++ src/components/Newnft.jsx
@@ -0,0 +1,82 @@
+import {Avatar, AvatarGroup, Box, Button, Grid, GridItem, HStack, Icon, Image, List, ListItem, Popover, PopoverBody, PopoverContent, PopoverTrigger, Text} from "@chakra-ui/react";
+import { LiaEllipsisHSolid, LiaHeart } from "react-icons/lia";
+
+const details = [
+  {
+    source: "/images/1.jpg",
+    title: "Pumpkin Head",
+    subtitle: "Highest bid 1/20"
+  },
+  {
+    source: "/images/2.jpg",
+    title: "Super Brain",
+    subtitle: "Highest bid 1/20"
+  },
+  {
+    source: "/images/3.jpg",
+    title: "Cool Cube",
+    subtitle: "Highest bid 1/20"
+  }
+];
+
+const showdetails = details.map((detail)=>{
+  return(
+    <>
+      <GridItem bg={"gray.800"} borderRadius={"md"} p={2} cursor={"pointer"}>
+        <Image w={"100%"} minH={"350px"} src={detail.source} objectFit={"cover"} borderRadius={"md"}/>
+        <Box p={2}>
+          {/* here we have the avatar group */}
+          <Box pt={2} display={"flex"} alignItems={"center"} justifyContent={"space-between"}>
+            <Box>
+              <AvatarGroup size={"sm"}>
+                <Avatar name="prof1" src="/images/prof1.png"></Avatar>
+                <Avatar name="prof1" src="/images/prof2.png"></Avatar>
+                <Avatar name="prof1" src="/images/prof3.png"></Avatar>
+              </AvatarGroup>
+            </Box>
+
+            <Box>
+              <Popover>
+                <PopoverTrigger>
+                  <Button p={0} variant={"ghost"} _hover={{background: "gray.700"}} _focus={{background: "gray.700"}}>
+                    <Icon color={"gray.400"} w={6} h={6} as={LiaEllipsisHSolid}/>
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent bg={"gray.700"} border={"none"}>
+                  <PopoverBody  >
+                    <List color={"white"}>
+                      <ListItem py={2} px={1} cursor={"pointer"} _hover={{bg: "gray.600", borderRadius: "md"}}>Share</ListItem>
+                      <ListItem py={2} px={1} cursor={"pointer"} _hover={{bg: "gray.600", borderRadius: "md"}}>Report</ListItem>
+                    </List>
+                  </PopoverBody>
+                </PopoverContent>
+              </Popover>
+            </Box>
+          </Box>
+          <Box>
+            <Text color={"white"} fontSize={"xl"} pt={2}>{detail.title}</Text>
+            <Text color={"gray.400"} py={1}>{detail.subtitle}</Text>
+          </Box>
+          <Box display={"flex"} justifyContent={"space-between"}>
+            <Text color={"blue.400"}>0.244wETH</Text>
+            <HStack>
+              <Icon as={LiaHeart} color={"white"}/>
+              <Text color={"white"}>322</Text>
+            </HStack>
+          </Box>
+        </Box>
+      </GridItem>
+    </>
+  )
+})
+export const Newnft = () => {
+  return (
+    <>
+        <Box pt={"20px"}>
+          <Grid templateColumns={{base: "repeat(1, 1fr)", md: "repeat(3, 1fr)"}} gap={8}>
+            {showdetails}
+          </Grid>
+        </Box>
+    </>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
-import { Box, Button, ButtonGroup, Center, Container, Grid, GridItem, Image, Text } from "@chakra-ui/react"
+import { Box, Button, ButtonGroup, Center, Container, Grid, GridItem, HStack, Icon, Image, Input, Text } from "@chakra-ui/react"
+import { LiaArrowRightSolid } from "react-icons/lia";
 import { Navbar } from "../components/Navbar"
 import { Categories } from "../components/Categories"
 import { Steps } from "../components/Steps"
+import { Newnft } from "../components/Newnft";
 
 export const Home = () => {
   return (
@@ -52,7 +54,7 @@ export const Home = () => {
             <GridItem>
               <Box>
                 <Box>
-                  <Image src="/images/NFT_portfolio.jpg" boxSize={"500px"} w={"100%"} objectFit={"cover"} borderRadius={"md"}/>
+                  <Image src="/images/portfolio_2.jpg" minH={"500px"} w={"100%"} objectFit={"cover"} borderRadius={"md"}/>
                 </Box>
               </Box>
             </GridItem>
@@ -61,12 +63,42 @@ export const Home = () => {
       </Container>
 
       {/* This section contains the steps ie step-01 */}
-      <Container maxW={"7xl"} pt={"30px"} pb={"50px"}>
+      <Container maxW={"7xl"} pt={"30px"} pb={"30px"}>
         <Steps/>
       </Container>
 
       {/* This is a break section */}
       <Box id="breaker"></Box>
+
+      {/* These are the NFT Cards */}
+      <Container maxW={"7xl"} py={"40px"}>
+        {/* this is the title for the cards */}
+        <Box display={"flex"} alignItems={"center"} justifyContent={"space-between"}>
+          <Text color={"white"} fontSize={"2xl"}>Newest Items</Text>
+          <HStack spacing={"5px"} cursor={"pointer"}>
+            <Text color={"gray.400"} fontSize={"md"}>VIEW ALL</Text>
+            <Icon color={"gray.400"} w={8} h={6} as={LiaArrowRightSolid}/>
+          </HStack>
+        </Box>
+
+        <Newnft/>
+      </Container>
+
+      {/* This is the newsletter section */}
+      <Container maxW={"7xl"} pb={"40px"}>
+        <Box id="newsletter" borderRadius={"md"}>
+          <Box minH={"50vh"} display={"flex"} alignItems={"center"} justifyContent={"center"} p={5}>
+            <Box w={{base: "100%", md: "60%"}}>
+              <Text textAlign={"center"} fontSize={{base: "xl", md: "3xl"}} color={"white"}>Join our newsletter</Text>
+              <Text fontSize={"xl"} color={"gray.400"} textAlign={"center"} py={2}>Weekly Free NFT updates straight to your inbox</Text>
+              <HStack>
+                <Input placeholder="Your Email" bg={"gray.800"} border={"none"} color={"white"} size={"lg"}/>
+                <Button size={"lg"} bg={"blue.400"} color={"white"} _hover={{color: "black", bg: "white"}} transition={"ease-in-out .7s"}>Subscribe</Button>
+              </HStack>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
     </>
   )
 }
